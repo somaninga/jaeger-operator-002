abfb098f4410d0927e4af2bbf7ebbcae4eb20020
somaninga_pujari <somaninga@paramountsoft.net>
2022-06-07 05:34:06 +0000

    [updated code]

--- service-a/hello.js
+++ service-a/hello.js
@@ -1,30 +1,37 @@
+const opentracing = require('opentracing')
+const tracer = opentracing.globalTracer()
 
 let counter = 1
 const sayHello = async (req, res) => {
   // Parse the handler input
+  const span = tracer.startSpan('say-hello', { childOf: req.span })
   const name = req.params.name
-
   // simulate a slow request every 3 requests
-  setTimeout(async () => {
-    const response = await formatGreeting(name);
-    // const response = await formatGreetingRemote(name, span)
-    res.send(response)
-  }, counter++ % 3 === 0 ? 100 : 0)
+  span.log({ event: 'name', message: `this is a log message for name ${name}` })
+  span.setBaggageItem('my-baggage', name)
+  const response = await formatGreetingRemote(name, span);
+  span.setTag('response', response)
+  span.finish()
+  res.send(response)
+
 }
 
 function formatGreeting(name, parent) {
+  const span = tracer.startSpan('format-greeting', { childOf: parent })
+  span.log({ event: 'format', message: `formatting message locally for name ${name}` })
   const response = `Hello ${name}!`
+  span.finish()
   return response
 }
 
 const bent = require('bent')
 
 const formatGreetingRemote = async (name, span) => {
-  const service = process.env.SERVICE_FORMATTER || 'localhost'
+  const service = process.env.SERVICE_FORMATTER || 'service-b'
   const servicePort = process.env.SERVICE_FORMATTER_PORT || '8081'
   const url = `http://${service}:${servicePort}/formatGreeting?name=${name}`
   const headers = {}
-
+  tracer.inject(span, opentracing.FORMAT_HTTP_HEADERS, headers)
   const request = bent('string', headers)
   const response = await request(url)
   return response
--- service-b/formatter.js
+++ service-b/formatter.js
@@ -1,11 +1,15 @@
-
+const opentracing = require('opentracing')
+const tracer = opentracing.globalTracer()
 
 function formatGreeting(req, res) {
   // Parse the handler input
+  const span = tracer.startSpan('format-greeting', { childOf: req.span })
   const name = req.query.name
-
+  span.log({ event: 'format', message: `formatting message remotely for name ${name}` })
   const response = `Hello from service-b ${name}!`
-
+  const baggage = span.getBaggageItem('my-baggage')
+  span.log({ event: 'baggage', message: `this is baggage ${baggage}` })
+  span.finish()
   res.send(response)
 }
 module.exports = formatGreeting
