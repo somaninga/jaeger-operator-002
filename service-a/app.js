1ab366b325a8252f63f63c42bf9894de6feb1262
somaninga_pujari <somaninga@paramountsoft.net>
2022-06-07 10:50:48 +0000

    updated host url

--- service-a/app.js
+++ service-a/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 8080
-const serviceName = process.env.SERVICE_NAME || 'service-a'
+const serviceName = process.env.SERVICE_NAME || 'service-a.jaeger-operator'
 
 // Initialize the Tracer
 const tracer = initTracer(serviceName)
--- service-b/app.js
+++ service-b/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 8081
-const serviceName = process.env.SERVICE_NAME || 'service-b'
+const serviceName = process.env.SERVICE_NAME || 'service-b.jaeger-operator'
 
 // Initialize the Tracer
 const tracer = initTracer(serviceName)
